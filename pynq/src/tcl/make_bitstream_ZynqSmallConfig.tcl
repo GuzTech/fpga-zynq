bfcb79fd70fcdbd54a6b28207ccdbca84e339a8b
Oguz Meteer <info@guztech.nl>
2016-12-17 21:28:20 +0100

    Initial Digilent PYNQ-Z1 commit
    Signed-off-by: Oguz Meteer <info@guztech.nl>

---
+++ pynq/src/tcl/make_bitstream_ZynqSmallConfig.tcl
@@ -0,0 +1,8 @@
+open_project pynq_rocketchip_ZynqSmallConfig/pynq_rocketchip_ZynqSmallConfig.xpr
+reset_run synth_1
+reset_run impl_1
+launch_runs synth_1
+wait_on_run synth_1
+launch_runs impl_1 -to_step write_bitstream
+wait_on_run impl_1
+exit
